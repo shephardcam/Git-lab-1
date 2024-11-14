8933eab6fd3bbf81dc5c9ef6de5391983c23b1a7
OneJuicyDoor <shephardcam@gmail.com>
2024-11-14 12:09:04 -0400

    Initial Upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picHelp = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.txtFutureValue = new System.Windows.Forms.TextBox();
+            this.lblRatePerYear = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpSolution = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblPaymentAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picHelp)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.grpSolution.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picHelp
+            // 
+            this.picHelp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picHelp.Image = global::Lab3.Properties.Resources.Help;
+            this.picHelp.Location = new System.Drawing.Point(275, 0);
+            this.picHelp.Name = "picHelp";
+            this.picHelp.Size = new System.Drawing.Size(91, 68);
+            this.picHelp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHelp.TabIndex = 0;
+            this.picHelp.TabStop = false;
+            this.picHelp.Click += new System.EventHandler(this.picHelp_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.txtYears);
+            this.groupBox1.Controls.Add(this.txtFutureValue);
+            this.groupBox1.Controls.Add(this.lblRatePerYear);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.picHelp);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 177);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data Required";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(260, 138);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 29);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(142, 138);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 29);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(11, 138);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 29);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(152, 103);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(110, 29);
+            this.txtYears.TabIndex = 6;
+            this.txtYears.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtFutureValue
+            // 
+            this.txtFutureValue.Location = new System.Drawing.Point(152, 31);
+            this.txtFutureValue.Name = "txtFutureValue";
+            this.txtFutureValue.Size = new System.Drawing.Size(110, 29);
+            this.txtFutureValue.TabIndex = 5;
+            this.txtFutureValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblRatePerYear
+            // 
+            this.lblRatePerYear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblRatePerYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRatePerYear.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRatePerYear.Location = new System.Drawing.Point(152, 69);
+            this.lblRatePerYear.Name = "lblRatePerYear";
+            this.lblRatePerYear.Size = new System.Drawing.Size(110, 24);
+            this.lblRatePerYear.TabIndex = 4;
+            this.lblRatePerYear.Text = "7.5";
+            this.lblRatePerYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Years:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rate per Year:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Future Value:";
+            // 
+            // grpSolution
+            // 
+            this.grpSolution.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.grpSolution.Controls.Add(this.label5);
+            this.grpSolution.Controls.Add(this.label4);
+            this.grpSolution.Controls.Add(this.lblPaymentAmount);
+            this.grpSolution.Location = new System.Drawing.Point(385, 12);
+            this.grpSolution.Name = "grpSolution";
+            this.grpSolution.Size = new System.Drawing.Size(227, 176);
+            this.grpSolution.TabIndex = 2;
+            this.grpSolution.TabStop = false;
+            this.grpSolution.Text = "Solution";
+            // 
+            // lblPaymentAmount
+            // 
+            this.lblPaymentAmount.Location = new System.Drawing.Point(10, 70);
+            this.lblPaymentAmount.Name = "lblPaymentAmount";
+            this.lblPaymentAmount.Size = new System.Drawing.Size(211, 29);
+            this.lblPaymentAmount.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Monthly Payments:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 42);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Payments are totalled.\r\n   Reset to start over";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(628, 208);
+            this.Controls.Add(this.grpSolution);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Annuity Payment by Cameron Shephard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picHelp)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpSolution.ResumeLayout(false);
+            this.grpSolution.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHelp;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblRatePerYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtFutureValue;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox grpSolution;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox lblPaymentAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        private decimal totalPayments = 0;
+
+        private const int MINYEARS = 5;
+
+        private const string name = "Cameron Shephard";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /* Name: Cameron Shephard
+* Date: october 27, 2024
+* This program calculates an annuity */
+
+        private void picHelp_Click(object sender, EventArgs e)
+        {
+            const string helpText = "This program reads in 2 numbers:\n\tFuture Value: amount of \"$\"\n\tYears: must be at least 5\nand calculates payments based on:\n\tYearly interest rate of 7.5%";
+            MessageBox.Show(helpText, name);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            string exitText = "Thanks for using the program!\nTotal payments = $" + totalPayments + ".";
+            MessageBox.Show(exitText, name);
+            Close();
+        }
+        /* hide solution groupbox reset text boxes and set cursor to top
+        */
+        private void ResetAll()
+        {
+            grpSolution.Hide();
+            txtFutureValue.Text = string.Empty;
+            txtYears.Text = string.Empty;
+            txtFutureValue.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetAll();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetAll();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Read input values
+                decimal futureValue = decimal.Parse(txtFutureValue.Text);
+                int years = int.Parse(txtYears.Text);
+
+                // Check if years is valid
+                if (years < MINYEARS)
+                {
+                    MessageBox.Show("Years must be at least " + MINYEARS + ".", name);
+                    txtYears.Text = string.Empty;
+                    txtYears.Focus();
+                    return;
+                }
+
+                // Constants
+                const decimal interestRate = 0.075m; // 7.5%
+
+                // Calculate the payment
+                decimal payment = (futureValue * (interestRate / 12)) / (decimal)(Math.Pow((double)(1 + (interestRate / 12)), years * 12) - 1);
+
+
+                // Display the result
+                lblPaymentAmount.Text = $"Payment Amount:" + Math.Round(payment, 2);
+
+                // Update total payments
+                totalPayments += payment;
+
+                // Show the solution group
+                grpSolution.Show();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Message encountered.\nInput string was not in a correct format." + ex.Message, name);
+                txtFutureValue.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error");
+            }
+        }
+
+    }
+}
